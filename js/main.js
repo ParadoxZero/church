2f8656e169bc19c1d9f02f40adb62661253e2e1e
Sidhin S Thomas <sidhin.thomas@gmail.com>
2018-06-21 21:50:16 +0530

    design fixed

--- js/main.js
+++ js/main.js
@@ -1,17 +1,27 @@
-document.addEventListener('DOMContentLoaded', function() {
-    //M.AutoInit();
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, {});
+function initMaterialCSS() {
+     //M.AutoInit();
+     var elems = document.querySelectorAll('.sidenav');
+     var instances = M.Sidenav.init(elems, {});
+ 
+     var elems = document.querySelectorAll('.fixed-action-btn');
+     var instances = M.FloatingActionButton.init(elems, {});
+ 
+     var elems = document.querySelectorAll('.tooltipped');
+     var instances = M.Tooltip.init(elems, {});
+ 
+     var elems = document.querySelectorAll('.parallax');
+     var instances = M.Parallax.init(elems, {});
+ 
+     var elems = document.querySelectorAll('.dropdown-trigger');
+     var instances = M.Dropdown.init(elems, {});
+}
+function initialize() {
+    initMaterialCSS();
+    $("#about").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#about-div").offset().top
+        }, 500);
+    });
+}
+document.addEventListener('DOMContentLoaded', initialize);
 
-    var elems = document.querySelectorAll('.fixed-action-btn');
-    var instances = M.FloatingActionButton.init(elems, {});
-
-    var elems = document.querySelectorAll('.tooltipped');
-    var instances = M.Tooltip.init(elems, {});
-
-    var elems = document.querySelectorAll('.parallax');
-    var instances = M.Parallax.init(elems, {});
-
-    var elems = document.querySelectorAll('.dropdown-trigger');
-    var instances = M.Dropdown.init(elems, {});
-  });
