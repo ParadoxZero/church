249796fd18bffb3a077662b1f243a0fb59a97af8
Sidhin S Thomas <sidhin.thomas@gmail.com>
2018-06-27 15:29:08 +0530

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,14 @@
-echo "Building The website..."
+GREEN='\033[0;32'
+RED='\033[0;31m'
+NC='\033[0m'
+
+echo "${GREEN}Building The website..."
 npm run build
 echo "Transfering files to gh-pages..."
 git checkout gh-pages
+if [ $? -neq 0 ]; then
+  echo "${RED}Checkout failed.. Aborting${NC}"
+fi
 cp -r dist/* .
 rm -r dist/*
 echo "Commiting new files..."
