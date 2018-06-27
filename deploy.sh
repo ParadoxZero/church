1f8c074fd28f447c7792716b20d91b46987be71f
Sidhin S Thomas <sidhin.thomas@gmail.com>
2018-06-27 15:20:09 +0530

    Updates: Wed 27 Jun 2018 15:20:09 IST

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,12 @@
+echo "Building The website..."
 npm run build
+echo "Transfering files to gh-pages..."
 git checkout gh-pages
 cp -r dist/* .
 rm -r dist/*
+echo "Commiting new files..."
 git add *
 git commit -m "Updates: $(date)" -a
 git push
 git checkout master
+echo "Deploy Complete."
