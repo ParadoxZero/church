909e3eb0bdc94e8236a1a166f11447210489f7fe
Sidhin S Thomas <thomas.sidhin@outlook.com>
2024-08-10 19:51:15 +0530

    Header UI feature parity

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,4 @@
-import { Affix, Card, Image, Menu, MenuProps } from 'antd';
+import { Affix, Card, Image, Menu, MenuProps, Button, Flex, Typography, Divider } from 'antd';
 import React from 'react';
 
 import avatar from '../assets/logo.png';
@@ -28,9 +28,44 @@ export class Header extends React.Component {
             </Card>
             <Menu mode="horizontal" 
             style={{justifyContent: 'flex-end', boxShadow:'0 2px 2px -2px grey',}}
-            items={items} />;
+            items={items} />
+           {this.render_side_nav()}
            </> 
         );
+    }
+
+    render_side_nav() {
+        return (
+            <div style={{position:'absolute', top:80, right:10, width: 200}}>
+                <Flex vertical gap={10}>
+                <Button shape='round' size='large' block>Vicars</Button>
+                <Button shape='round' size='large' block>Management</Button>
+                <Button shape='round' size='large' block>Ministries</Button>
+                <Button shape='round' size='large' block>Gallery</Button>
+                {this.render_news()}
+                </Flex>
+            </div>
+        )
+    }
+
+    render_news() {
+        const render_news_item = (info: string) => {
+            return (
+                <>
+                <Typography.Text style={{fontSize: 16, color: 'grey'}}>{info}</Typography.Text>
+                <Divider style={{padding:0, margin:0}} />
+                </>
+            )
+        }
+        const news_items = ['Sunday school to resume soon', 'New church building to be completed soon', 'New church website to be launched soon'];
+        return (
+            <Card title="Latest" extra={<a href="#">More</a>}>
+                <Flex vertical gap={10}>
+                {news_items.map((item) => render_news_item(item))}
+                </Flex>
+            </Card>
+        )
+
     }
 }
 
