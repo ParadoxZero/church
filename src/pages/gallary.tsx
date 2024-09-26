09e49002ddac4e06254f5a508173bf0235d65770
Sidhin S Thomas <thomas.sidhin@outlook.com>
2024-09-26 20:40:10 +0530

    Update style a little

--- src/pages/gallary.tsx
+++ src/pages/gallary.tsx
@@ -2,13 +2,13 @@ import { Card, Flex, Image } from "antd";
 import { getCurrentScreenSize, ScreenSize } from "../service/screen_size";
 
 export function Gallary(props: { url_list: string[] }) {
-    let screen_width = '75vw';
+    let screen_width = '50vw';
     if (getCurrentScreenSize() === ScreenSize.SMALL) {
         screen_width = '90vw';
     }
     return (
         <Flex style={{ width: '100vw' }} align="center" justify="center">
-            <Flex style={{ width: screen_width }} align="center" justify="stretch" wrap gap={15}>
+            <Flex style={{ width: screen_width, padding: 15 }} align="center" justify="stretch" wrap gap={15}>
                 {props.url_list.map((url, index) => (
                     <Image key={index} src={url} width={350} />
                 ))}
